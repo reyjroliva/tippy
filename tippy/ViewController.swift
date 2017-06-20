d2bedc5a952a99cca701326b8875c3723fc87494
Rey Oliva <reyjroliva@MacBook-Pro-203.dhcp.thefacebook.com>
2017-06-20 15:46:11 -0700

    Made the Tip and Total change dynamically with respect to bill

--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -24,5 +28,18 @@ class ViewController: UIViewController {
     @IBAction func onTap(_ sender: Any) {
         view.endEditing(true)
     }
+    
+    
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * 0.2
+        let total = bill + tip
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+    }
+    
 }
 
