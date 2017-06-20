0c5512405dc63f7560fd306f11f52d097bf4182b
Rey Oliva <reyjroliva@MacBook-Pro-203.dhcp.thefacebook.com>
2017-06-20 15:53:19 -0700

    Tip and Total now use 2 decimal places

--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -37,8 +37,8 @@ class ViewController: UIViewController {
         let tip = bill * 0.2
         let total = bill + tip
         
-        tipLabel.text = "$\(tip)"
-        totalLabel.text = "$\(total)"
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
     }
     
 }
